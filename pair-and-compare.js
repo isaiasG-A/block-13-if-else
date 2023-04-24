01cbd86e77e8c9802322f64bdee3d1d3a4ce690f
Isaias Garcia <isaias@Isaiass-MacBook-Air.local>
2023-04-24 10:44:09 -0700

    compound boolean expression has been implemented on pair-and-compare.js file

---
+++ pair-and-compare.js
@@ -0,0 +1,12 @@
+//If one set of data composed of numbers or strings or a combination of both, is the same as a second set of data, then print true. If the sets are not the same, print false.
+
+const set1A = "test";
+const set1B = 9;
+const set2A = "";
+const set2B = 9;
+
+if(set1A  === set2A && set1B === set2B) {
+  console.log(true);
+} else {
+  console.log(false);
+}
