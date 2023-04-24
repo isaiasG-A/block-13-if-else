77e1ff17f9347e8391b53adfe306171e1095efdc
Isaias Garcia <isaias@Isaiass-MacBook-Air.local>
2023-04-24 10:31:07 -0700

    compound boolean expression has been created on greater-than-5.js file

---
+++ greater-than-5.js
@@ -0,0 +1,10 @@
+const num1 = 6;
+const num2 = 4;
+
+//If number1 and number2 are both equal or greater than 5, then true should be printed. Otherwise, false should be printed.
+
+if(num1 && num2 >= 5) {
+  console.log(true);
+} else {
+  console.log(false);
+}
