93cf294e509f04288f09e40b0e2a428b9b5b109f
Isaias Garcia <isaias@Isaiass-MacBook-Air.local>
2023-04-28 20:13:57 -0700

    line 6 in greater-than-5.js file has been corrected to better follow comparison logic

--- greater-than-5.js
+++ greater-than-5.js
@@ -1,9 +1,9 @@
-const num1 = 6;
-const num2 = 4;
+const num1 = 1;
+const num2 = 6;
 
 //If number1 and number2 are both equal or greater than 5, then true should be printed. Otherwise, false should be printed.
 
-if(num1 && num2 >= 5) {
+if(num1 >=5 && num2 >= 5) {
   console.log(true);
 } else {
   console.log(false);
